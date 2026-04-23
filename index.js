c50e2b27892b5ddc5090fd4196b0cfa64b3579a9
Anshumita Sahu <anshumitasahu.official@gmail.com>
2026-04-23 13:21:23 +0530

    game, jokes and table here

---
+++ index.js
@@ -0,0 +1,54 @@
+// let navElement = document.querySelector("nav");
+// let navItems = document.getElementsByClassName("list");
+// nav = () => {
+//     console.log(navElement);
+//     navElement.style.backgroundColor = "red";
+// }
+// nav();
+
+// items = () => {
+//     console.log(navItems);
+//     for (let item of navItems) {
+//         item.style.backgroundColor = "pink";
+//     }
+// }
+// items();
+
+// document.getElementsByTagName("nav")[0].firstElementChild.style.color = "green";
+// document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green";
+// Array.from(document.getElementsByTagName("li")).forEach((element) => {
+//     element.style.backgroundColor = "cyan";
+// });
+// let choices = ['s', 'w', 'g'];
+// let cpu = choices[Math.floor(Math.random() * 3)];
+// console.log(cpu)
+// let user = prompt("Enter s, w, or g");
+// const match = (cpu, user) => {
+//     if (cpu === user) {
+//         return 0
+//     } else if (cpu === 's' && user === 'w') {
+//         return "cpu"
+//     } else if (cpu === 's' && user === 'g') {
+//         return "user"
+//     } else if (cpu === 'g' && user === 'w') {
+//        return "user"
+//     } else if (cpu === 'g' && user === 's') {
+//         return "cpu"
+//     } else if (cpu === 'w' && user === 's') {
+//         return "user"
+//     } else if (cpu === 'w' && user === 'g') {
+//         return "cpu"
+//     }
+// }
+// let result = match(cpu, user) 
+
+// document.body.innerHTML = `
+//     cpu: ${cpu} <br>
+//     user: ${user} <br>
+//     winner: ${result}
+// `;
+let divArray = Array.from(document.getElementsByClassName("joke"));
+console.log(divArray);
+divArray.forEach((div) => {div.style.display = "none"});
+let output = divArray[Math.floor(Math.random() * divArray.length)];
+output.style.display = "block";
